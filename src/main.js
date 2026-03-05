2e9bdc6c9cc127d4c81cb24944f2f1d15c8e4eaa
AlejandroVacaxX <erick.pintor@cua.uam.mx>
2026-03-05 00:27:51 -0600

    cambios minimos

--- src/main.js
+++ src/main.js
@@ -6,24 +6,29 @@ const PORT = process.env.PORT || 3000;
 
 const { ejecutarScriptPython } = require('./python/pythonApi.js');
 
-const iniciarApp = async () => {
-    console.log("Iniciando sistema...");
 
-    try {
-        console.log("Llamando a Python...");
-        // Esperamos a que Python termine y nos devuelva el texto
-        const respuestaPython = await ejecutarScriptPython(); 
-        
-        console.log("¡Éxito! Python dice:", respuestaPython);
-        
-        // Aqui mandamos respuesta al dom
-        
-    } catch (error) {
-        console.error("Hubo un problema al ejecutar Python:", error);
-    }
+// Esta madre se descomenta antes de la presentacion loko
+
+/*
+const iniciarApp = async () => {
+  console.log("Iniciando sistema...");
+
+  try {
+    console.log("Llamando a Python...");
+    // Esperamos a que Python termine y nos devuelva el texto
+    const respuestaPython = await ejecutarScriptPython();
+
+    console.log("¡Éxito! Python dice:", respuestaPython);
+
+    // Aqui mandamos respuesta al dom
+
+  } catch (error) {
+    console.error("Hubo un problema al ejecutar Python:", error);
+  }
 };
 
-iniciarApp();
+iniciarApp();*/
+
 
 // asi tendremos un middleware
 app.use(express.json())
@@ -33,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'principal.html'));
-  
+
 });
 
 // esta aun no la usare.
@@ -45,7 +50,7 @@ app.get('/api/camiones', (req, res) => {
     { id: 'TRK-002', chofer: 'Ana G.', estado: 'advertencia', velocidad: 110, combustible: 40 },
     { id: 'TRK-004', chofer: 'Luis P.', estado: 'mantenimiento', velocidad: 0, combustible: 10 }
   ];
-  
+
   res.json(camionesMock);
 });
 
@@ -60,7 +65,7 @@ app.post('/api/alertas', (req, res) => {
 
   // Adelante guardaremos en base de datos o enviaremos el correo.
   // Por ahora, solo le respondemos que todo salio al putazo.
-  
+
   res.status(200).json({
     status: 'success',
     message: 'Alerta procesada y guardada en el servidor correctamente',
@@ -68,6 +73,6 @@ app.post('/api/alertas', (req, res) => {
   });
 });
 
-app.listen(PORT,() =>{
-    console.log(`Servidor levantado en puerto http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor levantado en puerto http://localhost:${PORT}`);
 })
--- src/python/vision-dormido.py
+++ src/python/vision-dormido.py
@@ -4,10 +4,7 @@ import numpy as np
 import time
 from mediapipe.framework.formats import landmark_pb2
 import requests
-import time # con esta biblioteca sabre cuanto tiempo tuvo los ojos cerrados la persona
-
-
-
+import time  # con esta biblioteca sabre cuanto tiempo tuvo los ojos cerrados la persona
 
 
 # Acceder a solutions via mp.solutions (funciona en 0.10.x)
@@ -73,7 +70,7 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
 # --- 3. BUCLE PRINCIPAL (MAIN LOOP) ---
 def main():
-    # voy a usar estas variables para el tiempo 
+    # voy a usar estas variables para el tiempo
     tiempo_inicio_ojos_cerrados = None
     TIEMPO_LIMITE_SEGUNDOS = 2.0  # Si pasan 4 segundos, mandamos la alerta
     alerta_ya_enviada = False
@@ -81,16 +78,13 @@ def main():
     ojo_cerrado_Izquierdo = 0.0
     ojo_cerrado_Derecho = 0.0
     boca_abierta = 0.0
-    ojosCerrados = False
-    
-                
-    
+
     # Inicializar la cámara (0 suele ser la webcam por defecto)
     cap = cv2.VideoCapture(1)
 
     # Crear el detector dentro de un bloque 'with' para asegurar que se cierre bien
     with FaceLandmarker.create_from_options(options) as landmarker:
-        print("Iniciando cámara... presiona 'q' para salir.",flush=True)
+        print("Iniciando cámara... presiona 'q' para salir.", flush=True)
 
         while cap.isOpened():
             success, frame = cap.read()
@@ -146,8 +140,6 @@ def main():
                 for blendshape in face_blendshapes:
                     print(f"{blendshape.category_name}: {blendshape.score:.3f}")
                 """
-                
-                
 
                 for blendshape in face_blendshapes:
                     if blendshape.category_name == "eyeBlinkLeft":
@@ -161,29 +153,32 @@ def main():
                     ojosCerrados = True
                     texto_en_pantalla = "       Zzzzz.."
                     color_text = (0, 0, 255)  # color rojo
-                    
+
                     # Usamos flush=True para que Node lo vea al instante
-                    #print(f"Alerta de ojos Cerrados {ojo_cerrado_Derecho:.3f} {ojo_cerrado_Izquierdo:.3f}", flush=True)
-                    
+                    # print(f"Alerta de ojos Cerrados {ojo_cerrado_Derecho:.3f} {ojo_cerrado_Izquierdo:.3f}", flush=True)
+
                     #  Iniciar el cronometro si es la primera vez que cierra los ojos
                     if tiempo_inicio_ojos_cerrados is None:
                         tiempo_inicio_ojos_cerrados = time.time()
-                        
+
                     #  Calcular cuanto lleva dormido
                     tiempo_trascurrido = time.time() - tiempo_inicio_ojos_cerrados
-                    
-                    #  Revisar si ya pasaron los 4 segundos 
-                    if tiempo_trascurrido >= TIEMPO_LIMITE_SEGUNDOS and not alerta_ya_enviada:
+
+                    #  Revisar si ya pasaron los 4 segundos
+                    if (
+                        tiempo_trascurrido >= TIEMPO_LIMITE_SEGUNDOS
+                        and not alerta_ya_enviada
+                    ):
                         print("Alerta por Conductor mimido", flush=True)
                         datos_camion = {
-                            "id": "TRK-001", 
-                            "chofer": "Erick Alejandro", 
+                            "id": "TRK-001",
+                            "chofer": "Erick Alejandro",
                             "estado": "dormido",
-                            "timestamp": time.time()
+                            "timestamp": time.time(),
                         }
                         enviar_json_camiones(datos_camion)
                         alerta_ya_enviada = True
-                        
+
                 else:
                     # Si los ojos NO están cerrados (es decir, los abrió), reiniciamos todo
                     tiempo_inicio_ojos_cerrados = None
@@ -193,8 +188,6 @@ def main():
                     texto_en_pantalla = "Bostezando"
                     color_text = (255, 0, 0)  # color azul?
                     print(f"Conductor con Boca abierta {boca_abierta:3f}")
-
-                
 
                 cv2.putText(
                     frame,
@@ -219,34 +212,33 @@ def main():
     cv2.destroyAllWindows()
 
 
-
-def enviar_json_camiones( datos_camion):
+def enviar_json_camiones(datos_camion):
     """
     Envía un JSON mediante POST si el booleano es True.
     """
-    
-        # Asegúrate de usar la URL completa, incluyendo el dominio/localhost
-    url = 'http://localhost:3000/api/alertas'
-        
+
+    # Asegúrate de usar la URL completa, incluyendo el dominio/localhost
+    url = "http://localhost:3000/api/alertas"
+
     try:
-            # El parámetro 'json=' automáticamente convierte el diccionario a JSON 
-            # y añade el header 'Content-Type: application/json'
-            response = requests.post(url, json=datos_camion)
-            
-            # Esto lanza una excepción si el servidor responde con un error (ej. 404, 500)
-            response.raise_for_status() 
-            
-            print("Petición enviada con éxito:", response.status_code, flush=True)
-            return response.json() # Retorna la respuesta del servidor si es necesario
-            
+        # El parámetro 'json=' automáticamente convierte el diccionario a JSON
+        # y añade el header 'Content-Type: application/json'
+        response = requests.post(url, json=datos_camion)
+
+        # Esto lanza una excepción si el servidor responde con un error (ej. 404, 500)
+        response.raise_for_status()
+
+        print("Petición enviada con éxito:", response.status_code, flush=True)
+        return response.json()  # Retorna la respuesta del servidor si es necesario
+
     except requests.exceptions.RequestException as e:
-            print(f"Error al conectar con la API: {e}", flush=True)
-            return None
+        print(f"Error al conectar con la API: {e}", flush=True)
+        return None
 
 
-#flag = True
-#mi_payload = {"id": 45, "ruta": "Norte", "activo": True}
-#enviar_json_camiones(flag, mi_payload)
+# flag = True
+# mi_payload = {"id": 45, "ruta": "Norte", "activo": True}
+# enviar_json_camiones(flag, mi_payload)
 
 
 if __name__ == "__main__":
