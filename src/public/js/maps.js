29b3c4c0f52ab1f0b8dae2032c96731a50f64c9c
AlejandroVacaxX <erick.pintor@cua.uam.mx>
2026-03-31 23:36:45 -0600

    problema multiventana

--- src/public/js/maps.js
+++ src/public/js/maps.js
@@ -47,11 +47,7 @@ async function initMap() {
 }
 
 // Le pedimos la llave a nuestro propio servidor Node
-<<<<<<< HEAD
-fetch('/api/config/maps')
-=======
-fetch('/CargarMapa')
->>>>>>> fix_linux
+fetch('/cargarMapa')
     .then(response => response.json())
     .then(data => {
         // Creamos la etiqueta <script> dinámicamente
